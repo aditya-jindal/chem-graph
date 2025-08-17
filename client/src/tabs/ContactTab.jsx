be0191ce8ab061af522124f72ec394a7db142779
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2025-08-17 17:55:10 +0530

    changed author order

--- client/src/tabs/ContactTab.jsx
+++ client/src/tabs/ContactTab.jsx
@@ -6,46 +6,47 @@ const ContactTab = () => {
         For any further information, please contact the authors:
       </p>
       <div className="space-y-10">
-        <div className="border-l-4 border-gray-300 pl-6">
-          <p>
-            <strong>Dr. Clement J</strong><br />
-            Assistant Professor,<br />
-            School of Advanced Sciences, Department of Mathematics,<br />
-            Vellore Institute of Technology, Vellore.<br />
-            E-mail: <a href="mailto:clementjmail@gmail.com" className="text-blue-700 hover:underline">clementjmail@gmail.com</a>
-          </p>
-        </div>
+  <div className="border-l-4 border-gray-300 pl-6 text-left">
+    <p>
+      <strong>Mr. Kavin Jacob D</strong><br />
+      Research Scholar,<br />
+      School of Advanced Sciences, Department of Mathematics,<br />
+      Vellore Institute of Technology, Vellore.<br />
+      E-mail: <a href="mailto:kavinjacob@gmail.com" className="text-blue-700 hover:underline">kavinjacob@gmail.com</a>
+    </p>
+  </div>
 
-        <div className="border-l-4 border-gray-300 pl-6">
-          <p>
-            <strong>Mr. Kavin Jacob D</strong><br />
-            Research Scholar,<br />
-            School of Advanced Sciences, Department of Mathematics,<br />
-            Vellore Institute of Technology, Vellore.<br />
-            E-mail: <a href="mailto:kavinjacob@gmail.com" className="text-blue-700 hover:underline">kavinjacob@gmail.com</a>
-          </p>
-        </div>
+  <div className="border-l-4 border-gray-300 pl-6 text-left">
+    <p>
+      <strong>Dr. Clement J</strong><br />
+      Assistant Professor,<br />
+      School of Advanced Sciences, Department of Mathematics,<br />
+      Vellore Institute of Technology, Vellore.<br />
+      E-mail: <a href="mailto:clementjmail@gmail.com" className="text-blue-700 hover:underline">clementjmail@gmail.com</a>
+    </p>
+  </div>
 
-        <div className="border-l-4 border-gray-300 pl-6">
-          <p>
-            <strong>Dr. M Arockiaraj</strong><br />
-            Associate Professor,<br />
-            Department of Mathematics,<br />
-            Loyola College, Chennai.<br />
-            E-mail: <a href="mailto:marockiaraj@gmail.com" className="text-blue-700 hover:underline">marockiaraj@gmail.com</a>
-          </p>
-        </div>
+  <div className="border-l-4 border-gray-300 pl-6 text-left">
+    <p>
+      <strong>Dr. M Arockiaraj</strong><br />
+      Associate Professor,<br />
+      Department of Mathematics,<br />
+      Loyola College, Chennai.<br />
+      E-mail: <a href="mailto:marockiaraj@gmail.com" className="text-blue-700 hover:underline">marockiaraj@gmail.com</a>
+    </p>
+  </div>
 
-        <div className="border-l-4 border-gray-300 pl-6">
-          <p>
-            <strong>Mr. Aditya Jindal</strong><br />
-            B.TECH, Computer Science<br />
-            School of Computer Science and Engineering,<br />
-            Vellore Institute of Technology, Vellore.<br />
-            E-mail: <a href="mailto:adityajindal1509@gmail.com" className="text-blue-700 hover:underline">adityajindal1509@gmail.com</a>
-          </p>
-        </div>
-      </div>
+  <div className="border-l-4 border-gray-300 pl-6 text-left">
+    <p>
+      <strong>Mr. Aditya Jindal</strong><br />
+      B.TECH, Computer Science<br />
+      School of Computer Science and Engineering,<br />
+      Vellore Institute of Technology, Vellore.<br />
+      E-mail: <a href="mailto:adityajindal1509@gmail.com" className="text-blue-700 hover:underline">adityajindal1509@gmail.com</a>
+    </p>
+  </div>
+</div>
+
     </div>
   );
 };
